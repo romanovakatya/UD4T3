df5dced5589e691d93b950c1bf6ef182f49a30fe
Katia <katerina.bogrom@gmail.com>
2021-07-14 17:00:45 +0200

    initial commit

---
+++ src/actividad3/Actividad3App.java
@@ -0,0 +1,70 @@
+package actividad3;
+
+public class Actividad3App {
+
+	public static void main(String[] args) {
+		
+		// declaración de las variables,
+		int X = 0;
+		int Y = 2;
+		double N = 1000.00;
+		double M = -56.246;
+		
+		//mostramos valor de cada variable,
+		System.out.println("Variable X = " + X);
+		System.out.println("Variable Y = " + Y);
+		System.out.println("Variable N = " + N);
+		System.out.println("Variable M = " + M);
+		
+		//La suma X + Y,
+		System.out.println(X + " + " + Y + " = " + (X + Y));
+		
+		//La diferencia X – Y,
+		System.out.println(X + " - " + Y + " = " + (X - Y));
+		
+		//El producto X * Y,
+		System.out.println(X + " * " + Y + " = " + (X * Y));
+		
+		//El cociente X / Y,
+		System.out.println(X + " / " + Y + " = " + (X / Y));
+		
+		//El resto X % Y,
+		System.out.println(X + " % " + Y + " = " + (X % Y));
+		
+		//La suma N + M,
+		System.out.println(N + " + " + M + " = " + (N + M));
+		
+		//La diferencia N – M,
+		System.out.println(N + " - " + M + " = " + (N - M));
+		
+		//El producto N * M,
+		System.out.println(N + " * " + M + " = " + (N * M));
+		
+		//El cociente N / M,
+		System.out.println(N + " / " + M + " = " + (N / M));
+		
+		// El resto N % M,
+		System.out.println(N + " % " + M + " = " + (N % M));
+		
+		// La suma X + N,
+		System.out.println(X + " + " + N + " = " + (X + N));
+		
+		// El cociente Y / M,
+		System.out.println(Y + " / " + M + " = " + (Y / M));
+		
+		// El resto Y % M,
+		System.out.println(Y + " %" + M + " = " + (Y % M));
+		
+		// El doble de cada variable,
+		System.out.println(X + " * 2 = " + (X * 2));
+		System.out.println(Y + " * 2 = " + (Y * 2));
+		System.out.println(N + " * 2 = " + (N * 2));
+		System.out.println(M + " * 2 = " + (M * 2));
+		
+		// La suma de todas las variables,
+		System.out.println(X + " + " + Y + " + " + N + " + " + M + " = " + (X + Y + N + M));
+		
+		// El producto de todas las variables,
+		System.out.println(X + " * " + Y + " * " + N + " * " + M + " = " + (X * Y * N * M));
+	}
+}
